1a05e61d42248920a6b6c9b8880f29713e425afa
bigonil <luca.bigoni@gmail.com>
2025-04-24 12:32:55 +0200

    Provisionare cluster EKS over EC2

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "eu-east-1"
+}
+
+variable "cluster_name" {
+  default = "aiops-eks-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "max_capacity" {
+  default = 3
+}
+
+variable "min_capacity" {
+  default = 1
+}
