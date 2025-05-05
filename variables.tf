25f8cc1b255cfd98d6dd7b5fba3a61f70e86aaab
bigonil <luca.bigoni@gmail.com>
2025-05-05 13:23:16 +0200

    Update EKS Cluster creation with IAM Role

--- variables.tf
+++ variables.tf
@@ -1,23 +1,34 @@
 variable "aws_region" {
-  default = "us-east-1"
+  type    = string
+  default = "us-west-2"
 }
 
 variable "cluster_name" {
+  type    = string
   default = "aiops-eks-cluster"
 }
 
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
 variable "node_instance_type" {
+  type    = string
   default = "t3.medium"
 }
 
 variable "desired_capacity" {
+  type    = number
   default = 2
 }
 
 variable "max_capacity" {
+  type    = number
   default = 3
 }
 
 variable "min_capacity" {
+  type    = number
   default = 1
 }
