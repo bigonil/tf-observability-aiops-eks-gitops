5137799e8f5ac9ac6713ab96ef8e41da019b79e4
bigonil <luca.bigoni@gmail.com>
2025-05-06 20:26:57 +0200

    Add route53_zone_id variables

--- variables.tf
+++ variables.tf
@@ -5,7 +5,6 @@ variable "aws_region" {
 
 variable "cluster_name" {
   type    = string
-  default = "aiops-eks-cluster"
 }
 
 variable "environment" {
@@ -31,4 +30,27 @@ variable "max_capacity" {
 variable "min_capacity" {
   type    = number
   default = 1
+}
+
+# Add variable definitions here
+
+variable "route53_zone_id" {
+  description = "The ID of the Route53 hosted zone"
+  type        = string
+}
+
+variable "email" {
+  description = "The email address for notifications"
+  type        = string
+}
+
+
+variable "issuer_name" {
+  description = "The name of the certificate issuer"
+  type        = string
+}
+
+variable "acme_server" {
+  description = "The ACME server URL for Let's Encrypt"
+  type        = string
 }
